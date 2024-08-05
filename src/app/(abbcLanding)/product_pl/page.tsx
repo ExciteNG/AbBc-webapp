e1525c4cfca2a97cfbdbd62273c5464d5442cc71
byron <byronayalogu@gmail.com>
2024-08-05 18:04:41 +0100

    initial layout and application structure

---
+++ src/app/(abbcLanding)/product_pl/page.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export const ProductPipeLine = ()=>{
+    return (
+        <section> product pipe line</section>
+    )
+};
+
+export default ProductPipeLine;
---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Button } from '../ui/button';
+import abbcLogo from '../../../public/assets/AbBC logo full color 1.svg'
+
+export const Header = () => {
+  return (
+    <header className='w-screen'>
+        <nav className='bg-slate-50 w-full px-2 md:px-6 lg:px-10 py-3 flex justify-between items-center'>
+            <div>
+              <Image src={abbcLogo} alt='abbc_logo' width={130} height={60} className='w-20 md:w-28'/>
+            </div>
+            <div className='hidden w-[60%] md:flex md:justify-between'>
+                <div className='text-primary text-xs md:text-sm lg:text-base focus:font-semibold hover:font-semibold cursor-pointer'>
+                    <Link href="/">Home</Link></div>
+                <div className='text-primary text-xs md:text-sm lg:text-base focus:font-semibold hover:font-semibold cursor-pointer'>
+                    <Link href="/about">About</Link></div>
+                <div className='text-primary text-xs md:text-sm lg:text-base focus:font-semibold hover:font-semibold cursor-pointer'>
+                    <Link href="/technology">Technology</Link></div>
+                <div className='text-primary text-xs md:text-sm lg:text-base focus:font-semibold hover:font-semibold cursor-pointer'>
+                    <Link href="/product_pl">Product & Pipeline</Link></div>
+                <div className='text-primary text-xs md:text-sm lg:text-base focus:font-semibold hover:font-semibold cursor-pointer'>
+                    <Link href="/news">News</Link></div>
+            </div>
+            <div>
+                <Button className='bg-primary'><Link href={'/contact'}>Contact</Link></Button>
+            </div>
+        </nav>
+    </header>
+  )
+};
+
+export default Header;

e8e3cec0d45a6be4ad602575a40ce4089c7bbe67
oluwayelu <oluwayeluifeoluwa@gmail.com>
2024-08-07 16:31:04 +0100

    feat: started the news page

---
+++ src/app/(abbcLanding)/news/[slug]/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const NewsSlug = () => {
+  return (
+    <div>
+      <div className="relative w-full h-[60dvh] bg-primary bg-cover bg-center">
+        <div className="w-full h-full px-5 md:px-10 flex flex-col justify-center gap-2 text-white">
+          <h1 className="font-extrabold text-4xl md:text-6xl">
+            Geneticus Breakthrough Revolutionizes DNA Sequencing Technology.
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NewsSlug;
